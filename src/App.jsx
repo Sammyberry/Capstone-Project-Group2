ed19ee4c94af0f3e7b7f4f32d0588a9d0def7d12
Samuel Akinbolawa <samuel4weezy@yahoo.com>
2026-05-26 15:28:13 +0100

    chore: initialize React project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <header>Header</header>
+      <main>
+        <section>Hero Section</section>
+        <section>Planet Section</section>
+        <section>Contact Form</section>
+      </main>
+      <footer>Footer</footer>
+    </>
+  );
+}
+
+export default App;
