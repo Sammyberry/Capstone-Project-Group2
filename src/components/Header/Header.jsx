c2eb28aa27d48a29c332df99f93e9c4d22b6d42a
k-coder85 <kingdazzlin8@gmail.com>
2026-05-29 23:10:31 +0100

    Header.jsx,Header.css, Hero.jsx and Hero.css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+ import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="graphics.png" alt="Logo" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,37 +1,42 @@
-import react from "react";
-import "./Hero.css";
+ import "./Hero.css";
+
 const Hero = () => {
   return (
-    <div className="hero">
-        <div className="hero-text">
-            <h1>Explore Our Solar <br> </br>
-            System Through Data
-            </h1>
-            
-             <p> 
-            Understand the planets not just by name, but by measurable<br></br>
-            facts. From size and mass to gravity and density, this page<br></br>
-             breaks down the solar system in a clear, data-driven way.
-             </p>
+    <section className="hero">
 
-             <div className="buttons">
-                <button className="explore-btn">
-                    Explore the Data
-                </button>
-                <button className="contact-btn">
-                    Contact Us
-                </button>
-                </div>
-        </div>   
-          
-            <div className="hero-image">
-            <img src="" alt="Earth">
-            </img>
-            </div>
-    </div>
-  )
-}
+      <div className="hero-text">
+        <h1>
+          Explore Our Solar <br />
+          System Through Data
+        </h1>
+
+        <p>
+          Understand the planets not just by name, but by measurable
+          facts. From size and mass to gravity and density, this page
+          breaks down the solar system in a clear, data-driven way.
+        </p>
+
+        <div className="buttons">
+          <button className="explore-btn">
+            Explore the Data
+          </button>
+
+          <button className="contact-btn">
+            Contact Us
+          </button>
+        </div>
+      </div>
+
+      <div className="hero-image-container">
+        <img
+          className="hero-image"
+          src="21fbbe77-184a-4cce-8c3c-fd483dc76e0a 1.png"
+          alt="Earth"
+        />
+      </div>
+
+    </section>
+  );
+};
+
 export default Hero;
-
-        
-    
