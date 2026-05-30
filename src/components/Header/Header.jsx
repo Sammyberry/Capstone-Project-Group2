03841bbd67fe5e9cd929de5397d76873a7229d4e
KING The Developer <kingdazzlin8@gmail.com>
2026-05-30 15:32:24 +0100

    created component structure for the project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,1 @@
- import "./Header.css";
- import logo from "./graphics.png";
 
-const Header = () => {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="graphics.png" alt="Logo" />
-      </div>
-    </header>
-  );
-};
-
-export default Header;
