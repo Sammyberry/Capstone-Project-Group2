3943975632c94251c25d9cf5545467b9202f751d
KING The Developer <kingdazzlin8@gmail.com>
2026-05-30 14:19:52 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+
 function App() {
   return (
     <>
-      <header>Header</header>
-      <main>
-        <section>Hero Section</section>
-        <section>Planet Section</section>
-        <section>Contact Form</section>
-      </main>
-      <footer>Footer</footer>
+      <Header />
+      <Hero />
     </>
   );
 }
