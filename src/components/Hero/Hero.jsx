80c6dade82211e1d806069151dd2376f33b80db5
KING The Developer <kingdazzlin8@gmail.com>
2026-05-30 14:10:56 +0100

    Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
  import "./Hero.css";
+ import earth from "./earth.png";
 
 const Hero = () => {
   return (
@@ -11,8 +12,8 @@ const Hero = () => {
         </h1>
 
         <p>
-          Understand the planets not just by name, but by measurable
-          facts. From size and mass to gravity and density, this page
+          Understand the planets not just by name, but by measurable<br/>
+          facts. From size and mass to gravity and density, this page<br/>
           breaks down the solar system in a clear, data-driven way.
         </p>
 
@@ -30,7 +31,7 @@ const Hero = () => {
       <div className="hero-image-container">
         <img
           className="hero-image"
-          src="21fbbe77-184a-4cce-8c3c-fd483dc76e0a 1.png"
+          src={earth}
           alt="Earth"
         />
       </div>
