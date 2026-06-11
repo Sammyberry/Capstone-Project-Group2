30439a672ec756f56c20044eb7185f97ef949dd1
Your Name <you@example.com>
2026-06-10 21:04:17 -0400

    updated theCcontactForm section

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,9 @@ import ContactForm from "./components/ContactForm/ContactForm";
 function App() {
   return (
     <>
-      <header>Header</header>
-      <main>
-        <section>Hero Section</section>
-        <section>Planet Section</section>
-        <section>
+      
           <ContactForm />
-        </section>
-      </main>
-      <footer>Footer</footer>
+        
     </>
   );
 }
