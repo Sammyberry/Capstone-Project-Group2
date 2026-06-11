efa64a473b4d3a17272532e8827d91ab13ebb96f
Samuel Akinbolawa <samuel4weezy@yahoo.com>
2026-06-11 14:22:54 +0100

    chore: integrate all sections into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import VideoSection from "./components/Video/Video";
+import ImageSection from "./components/Image/Image";
+import PlanetTable from "./components/PlanetTable/PlanetTable";
 import ContactForm from "./components/ContactForm/ContactForm";
+import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
     <>
-      
-          <ContactForm />
-        
+      <Header />
+      <Hero />
+
+      <VideoSection />
+
+      <ImageSection />
+
+      <PlanetaryTable />
+
+      <ContactForm />
+
+      <Footer />
     </>
   );
 }
